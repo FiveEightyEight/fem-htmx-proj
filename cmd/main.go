ea2381d724af9914dc7aada9ccb1e61368f256ef
Robert Abreu <robert.abreu3@gmail.com>
2024-02-03 18:40:12 -0500

    added some error handling for dup emails, new contacts send over the new contact html, instead of the full contact list

--- cmd/main.go
+++ cmd/main.go
@@ -28,6 +28,23 @@ type ContactData struct {
 	Contacts Contacts
 }
 
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+type Page struct {
+	Data ContactData
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: createNewData(),
+		Form: newFormData(),
+	}
+}
+
 func newContact(name string, email string) Contact {
 	return Contact{
 		Name:  name,
@@ -44,6 +61,13 @@ func createNewData() ContactData {
 	}
 }
 
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
@@ -55,28 +79,50 @@ func newTemplate() *Templates {
 	}
 }
 
+func (c ContactData) hasEmail(email string) bool {
+	for _, contact := range c.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 
 	// explicit type declaration of contactList
-	var contactList ContactData
-	contactList = createNewData()
+	// var contactList ContactData
+	// contactList = createNewData()
+
 	// OR
 	// implicit type declaration of contactList
 	// contactList := createNewData()
+	page := newPage()
 	e.Renderer = newTemplate()
 	e.GET("/", func(c echo.Context) error {
 		// http.StatusOK
-		return c.Render(200, "index", contactList)
+		return c.Render(200, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
-		contactList.Contacts = append(contactList.Contacts, newContact(name, email))
-		return c.Render(200, "index", contactList)
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already in use"
+			return c.Render(422, "form", formData)
+		}
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		c.Render(200, "form", newFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":6969"))
